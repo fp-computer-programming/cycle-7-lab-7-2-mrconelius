c04b4cab773f19f9978af11dad4a5f16c560db48
mrconelius <144148053+mrconelius@users.noreply.github.com>
2023-12-11 17:12:33 -0500

    Add files via upload

---
+++ lab_7-2.py
@@ -0,0 +1,18 @@
+def find_sum(num1, num2):
+    """
+    This function takes two numbers, num1 and num2,
+    adds them, and returns the sum.
+    """
+    # Adding num1 and num2
+    num_sum = num1 + num2
+
+    # Returning the sum
+    return num_sum
+
+# Calling the function with arguments 111 and 222
+my_sum = find_sum(111, 222)
+
+# Printing the result
+print(my_sum)
+
+# The output is 333 because 111 + 222 equals 333, and that result is stored in the variable my_sum and printed. 
